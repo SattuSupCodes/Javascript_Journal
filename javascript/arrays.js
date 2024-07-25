cd99af1aceccb064f0d71add92c6a33e047e7539
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-07-25 20:09:42 +0530

    update

---
+++ javascript/arrays.js
@@ -0,0 +1,13 @@
+//we learn about arrays and loops
+
+const array_one = ["apples" , "oranges" , "pineapples" , "kiwis"]
+console.log(array_one[3]) //indexing from 0 to n
+
+let size = array_one.length
+console.log(size)
+
+let size_fruit = array_one[2].length
+console.log(size_fruit)
+
+
+console.log(array_one)
