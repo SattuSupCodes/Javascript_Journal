047dafd29108afd8be9d66911d63a348bd739398
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-07-30 18:35:27 +0530

    upd

--- javascript/advancedFunctions.js
+++ javascript/advancedFunctions.js
@@ -2,4 +2,11 @@ function hello(){
     console.log("hello");
 }
 
-hello()
+hello();
+
+//functions as values
+
+const function1 = function hello(){
+    console.log("This is patrick")
+};
+console.log(typeof function1)
