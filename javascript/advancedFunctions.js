b70c8e224ca5d2ab380bf1f3ae6e025ccbec0197
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-10-08 14:34:30 +0530

    upd

--- javascript/advancedFunctions.js
+++ javascript/advancedFunctions.js
@@ -10,4 +10,4 @@ const function1 = function(){
     console.log("This is patrick")
 }; //anonymous function: one without a name
 console.log(typeof function1)
-//javascript makes me wanna kill myself (every language does actually)
+//javascript makes me wanna kill myself (every language does actually smh)
